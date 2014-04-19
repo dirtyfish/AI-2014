51a4845ba945bf43c4226b71317b6e8173624ed4
Espen <espenvh@gmail.com>
2014-04-19 17:09:07 +0200

    Better resize functions :)

--- assign3_00_noscreen_function.py
+++ assign3_00_noscreen_function.py
@@ -11,9 +11,9 @@ done in BlitzBasic. I was just translating the BlitzBasic code to
 pygame to compare the results. I didn't bother porting the text and
 sound stuff, that's an easy enough challenge for the reader :]"""
 
-import pygame, os, random, copy, pickle
+import pygame, os, random, copy
 from pygame.locals import *
-from math import sin
+
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 black = (255,255,255) #almost white
@@ -23,13 +23,40 @@ letters= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R
 imagenamelist=[]
 bitmaplist=[]
 
+def file30x30_2_dataset_as2d(filename):
+  result=[]
+  imagename= os.path.join(main_dir, 'RESIZED_30x30',filename)
+  bitmap = pygame.image.load(imagename)
+  da30list=[]
+  da30x30list=[]
+
+
+  #building 30x30 2d list
+  for x in range(30):
+       da30list.append(0)
+  for x in range(30):
+       da30x30list.append(copy.deepcopy(da30list))
+
+  #filling list with bitmap values
+  nr=0
+  for y in range(30):
+        for x in range (30):
+
+             pickcolor=bitmap.get_at((x,y))
+             da30x30list[nr/30][nr%30]=pickcolor[0]
+             nr+=1
+
+
+  return da30x30list
+
+
+
+
+
 
 def getrandomfilename():
     return letters[random.randint(0,len(letters)-1)]+str(random.randint(1,10))+".jpg"
-
-
-class Foo:
-    attr = 'a class attr'
+    
 
 
 
@@ -46,7 +73,7 @@ def main():
     for x in range(5):
       imagename= os.path.join(main_dir, 'RESIZED_30x30',getrandomfilename())
       bitmap = pygame.image.load(imagename)
-      print pickle.dumps(pygame.bitmap.get_at)
+      
 
       #bitmap = pygame.transform.scale2x(bitmap)
       #bitmap = pygame.transform.scale2x(bitmap)
@@ -159,5 +186,100 @@ def main():
         mainClock.tick(30)
 
 
+def filterdataset(set, filterlist):
+   resultlist=[]
+   rl=[]
+   for x in range(len(set)):
+      rl.append(0)
+   for x in range(len(set)):
+      resultlist.append(copy.deepcopy(rl))
+  
+
+   for x in range(len(set)):
+      for y in range(len(set[x])):
+        for value in filterlist:
+          resultlist[x][y]+=(set[x][y]<value)
+
+   return resultlist
+
+def resize_dataset(dataset,size):
+     setlength=len(dataset)
+     dasizexsizelist=[]
+     dasizelist=[]
+     for x in range(size):
+       dasizelist.append(0)
+     for x in range(size):
+       dasizexsizelist.append(copy.deepcopy(dasizelist))
+
+     numlist=copy.deepcopy(dasizexsizelist)
+     reslist=copy.deepcopy(dasizexsizelist)
+
+
+     if len(dataset)==size:
+      return dataset
+
+     else:
+      if len(dataset)<=size:
+         #print "to big"
+         for y in xrange(size):
+           for x in xrange(size):
+             reslist[y][x]=dataset[y*setlength/size][x*setlength/size]
+
+      else:
+         for y in xrange(setlength):
+           for x in xrange(setlength):
+              
+              dasizexsizelist[y*size/setlength][x*size/setlength]+=dataset[y][x]
+              numlist[y*size/setlength][x*size/setlength]+=1
+
+         for y in range(size):
+           for x in range(size):
+              reslist[x][y]=dasizexsizelist[x][y]/numlist[x][y]
+
+     return reslist
+     #return [reslist,numlist]
+
+
+
+print "hi this is not main"
+
+if 1:
+  def main():
+    print "hi this is the alternative main"
+    
+    imagename= os.path.join(main_dir, 'RESIZED_30x30',getrandomfilename())
+    print imagename
+    
+    mydataset=file30x30_2_dataset_as2d(imagename)
+    print mydataset
+
+    #resize dataset 
+    my90x90set=resize_dataset(mydataset,90)
+    my9x9set=resize_dataset(my90x90set,9)
+    
+    for fdat in filterdataset(resize_dataset(mydataset,90),[128]):
+       print str(fdat)[1:270:3]
+
+   
+    #resizing to smaller dataset might give a blur effect
+    #so you might wanna set filterlevel higher..
+    print my9x9set
+    for fdat in filterdataset(my9x9set,[180]):
+         #print fdat
+         print str(fdat)[1:27:3]
+
+
+    
+
+    
+    fdataset=filterdataset(mydataset,[128])
+
+    for underset in fdataset:
+      #print str(underset)[1:80:3] for sending in facebook
+     print str(underset)[1:90:3]
+
+
+
 
 if __name__ == '__main__': main()
+print __file__, "<---- avslutter"
